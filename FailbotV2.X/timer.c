d54ff5271402122c28880b030e729e3a55c08010
Table4 <table4@univ-tln.fr>
2021-11-16 17:50:33 +0100

    Presque autonome

--- FailbotV2.X/timer.c
+++ FailbotV2.X/timer.c
@@ -3,6 +3,9 @@
 #include "IO.h"
 #include "PWM.h"
 #include "ADC.h"
+#include "main.h"
+
+unsigned long timestamp = 0;
 
 //Initialisation d?un timer 32 bits
 void InitTimer23(void) {
@@ -26,6 +29,7 @@ int toggle = 0;
 //Interruption du timer 32 bits sur 2-3
 void __attribute__((interrupt, no_auto_psv)) _T3Interrupt(void) {
 IFS0bits.T3IF = 0; // Clear Timer3 Interrupt Flag
+/*
 if (toggle == 0)
 {
     PWMSetSpeedConsigne(30.0, MOTEUR_GAUCHE);
@@ -38,6 +42,7 @@ else
     PWMSetSpeedConsigne(-30.0, MOTEUR_DROIT);
     toggle = 0;
 }
+*/
 }
 
 //Initialisation d?un timer 16 bits
@@ -45,17 +50,37 @@ void InitTimer1(void)
 {
 //Timer1 pour horodater les mesures (1ms)
 T1CONbits.TON = 0; // Disable Timer
-T1CONbits.TCKPS = 0b10; //Prescaler
+SetFreqTimer1(50);
+//T1CONbits.TCKPS = 0b10; //Prescaler
 //11 = 1:256 prescale value
 //10 = 1:64 prescale value
 //01 = 1:8 prescale value
 //00 = 1:1 prescale value
 T1CONbits.TCS = 0; //clock source = internal clock
-PR1 = 0b0110000110101000;
+//PR1 = 0b0110000110101000;
 
 IFS0bits.T1IF = 0; // Clear Timer Interrupt Flag
 IEC0bits.T1IE = 1; // Enable Timer interrupt
 T1CONbits.TON = 1; // Enable Timer
+}
+
+//Initialisation d?un timer 16 bits
+void InitTimer4(void)
+{
+//Timer1 pour horodater les mesures (1ms)
+T4CONbits.TON = 0; // Disable Timer
+SetFreqTimer4(1000);
+//T1CONbits.TCKPS = 0b10; //Prescaler
+//11 = 1:256 prescale value
+//10 = 1:64 prescale value
+//01 = 1:8 prescale value
+//00 = 1:1 prescale value
+T4CONbits.TCS = 0; //clock source = internal clock
+//PR1 = 0b0110000110101000;
+
+IFS1bits.T4IF = 0; // Clear Timer Interrupt Flag
+IEC1bits.T4IE = 1; // Enable Timer interrupt
+T4CONbits.TON = 1; // Enable Timer
 }
 
 //Interruption du timer 1
@@ -64,4 +89,60 @@ void __attribute__((interrupt, no_auto_psv)) _T1Interrupt(void)
 IFS0bits.T1IF = 0;
 PWMUpdateSpeed();
 ADC1StartConversionSequence();
+}
+
+//Interruption du timer 4
+void __attribute__((interrupt, no_auto_psv)) _T4Interrupt(void)
+{
+IFS0bits.T1IF = 0;
+timestamp = timestamp+1;
+OperatingSystemLoop();
+}
+
+void SetFreqTimer1(float freq)
+{
+T1CONbits.TCKPS = 0b00; //00 = 1:1 prescaler value
+if(FCY /freq > 65535)
+{
+T1CONbits.TCKPS = 0b01; //01 = 1:8 prescaler value
+if(FCY /freq / 8 > 65535)
+{
+T1CONbits.TCKPS = 0b10; //10 = 1:64 prescaler value
+if(FCY /freq / 64 > 65535)
+{
+T1CONbits.TCKPS = 0b11; //11 = 1:256 prescaler value
+PR1 = (int)(FCY / freq / 256);
+}
+else
+PR1 = (int)(FCY / freq / 64);
+}
+else
+PR1 = (int)(FCY / freq / 8);
+}
+else
+PR1 = (int)(FCY / freq);
+}
+
+void SetFreqTimer4(float freq)
+{
+T1CONbits.TCKPS = 0b00; //00 = 1:1 prescaler value
+if(FCY /freq > 65535)
+{
+T1CONbits.TCKPS = 0b01; //01 = 1:8 prescaler value
+if(FCY /freq / 8 > 65535)
+{
+T1CONbits.TCKPS = 0b10; //10 = 1:64 prescaler value
+if(FCY /freq / 64 > 65535)
+{
+T1CONbits.TCKPS = 0b11; //11 = 1:256 prescaler value
+PR1 = (int)(FCY / freq / 256);
+}
+else
+PR1 = (int)(FCY / freq / 64);
+}
+else
+PR1 = (int)(FCY / freq / 8);
+}
+else
+PR1 = (int)(FCY / freq);
 }
